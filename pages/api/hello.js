b8bf85b6d7e885a5606c11bdc4cf4e3990ca1dfb
Cicero, Feijo <cicero.feijo@gmail.com>
2023-05-10 17:49:20 -0300

    Add API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 export const config = {
     runtime: "experimental-edge",
-  };
+};
   
-  export default async function (req) {
+export default async function (req) {
     return new Response(JSON.stringify({ name: "John Doe" }), {
       status: 200,
       headers: {
