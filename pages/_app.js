8d507023a6302aa0b84f3901934f6b39c21385c9
instrutor-feijo <ec2-user@ip-10-11-203-135.ap-southeast-1.compute.internal>
2023-05-10 00:07:35 +0000

    Pushed from Locofy Builder

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Head from "next/head";
-import { ThemeProvider, createTheme } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-
 import "./global.css";
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
-  const muiTheme = createTheme();
-
+function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
@@ -28,16 +12,9 @@ export default function MyApp(props) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <ThemeProvider theme={muiTheme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </React.Fragment>
   );
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
